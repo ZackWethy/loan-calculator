9b8e13b48574f54507767b2b38f1a3ef6ed3dcc6
Zack Wethy <zackwethy@zacks-mbp.mynetworksettings.com>
2024-08-25 21:07:27 -0400

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/globals.css';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HelmetProvider } from 'react-helmet-async';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
-// import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <Router>
-        <App />
-        <Analytics />
-        {/* <SpeedInsights /> */}
-      </Router>
-    </HelmetProvider>
+    <App />
+    <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
 
